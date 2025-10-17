0df125f0e7f86f235d95e04b15e6c062280bd2d1
DCabana98 <dcabanaramos@gmail.com>
2025-10-18 00:19:38 +0100

    fix: Corregido el path de content en tailwind.config.js para Vercel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  // RUTA ROBUSTA: Asegura que escanea todos los archivos React (.jsx, .tsx) 
+  // en la carpeta src y el index.html principal.
   content: [
     "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}"
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
